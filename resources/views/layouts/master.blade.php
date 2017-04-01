da7c1bcea8cfd2198868b7689a80291ef29909cc
leandroargentiero <argentieroleo@gmail.com>
2017-04-01 21:30:19 +0200

    Added necessary assets

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta name="keywords" content="Apptite" />
+    <meta name="description" content="Apptite - Het geheime ingrediënt voor een gloednieuwe eetervaring">
+    <meta name="author" content="Apptite Thomas More">
+
+    <!-- Title -->
+    <title>
+        @yield('title)- Apptite
+    </title>
+
+    <!-- Mobile Meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="favicon.png" type="image/x-icon">
+
+    <!-- CSS -->
+    <link href="assets/lib/bootstrap/css/bootstrap.css" type="text/css" rel="stylesheet" />
+    <link href="assets/css/colors.css" type="text/css" rel="stylesheet" />
+    <link href="assets/css/font.css" type="text/css" rel="stylesheet" />
+    <link href="assets/lib/jQuery.filer-master/css/jquery.filer.css" type="text/css" rel="stylesheet" />
+    <link href="assets/lib/jQuery.filer-master/css/themes/jquery.filer-dragdropbox-theme.css" type="text/css" rel="stylesheet" />
+    <link href="assets/lib/Magnific-Popup-master/magnific-popup.css" type="text/css" rel="stylesheet" />
+    <link href="assets/lib/popup/css/component.css" type="text/css" rel="stylesheet" />
+    <link href="assets/lib/icon8/styles.min.css" type="text/css" rel="stylesheet" />
+    <link href="assets/lib/font-awesome/css/font-awesome.min.css" type="text/css" rel="stylesheet" />
+    <link type="text/css" rel="stylesheet" href="assets/lib/jquerym.menu/css/jquery.mmenu.all.css" />
+    <link href='assets/css/mapbox.css' rel='stylesheet' />
+    <link href='assets/lib/chosen/chosen.css' rel='stylesheet' />
+    <link href='https://api.mapbox.com/mapbox.js/plugins/leaflet-markercluster/v0.4.0/MarkerCluster.css' rel='stylesheet' />
+    <link href='https://api.mapbox.com/mapbox.js/plugins/leaflet-markercluster/v0.4.0/MarkerCluster.Default.css' rel='stylesheet' />
+    <link href="assets/css/main.css" type="text/css" rel="stylesheet" />
+
+    <!-- IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+
+
+
+<!--==================================Javscript=================================-->
+<script type="text/javascript" src="assets/js/jquery-lib.js"></script><!-- Jquery Library -->
+<script type="text/javascript" src="assets/js/jquery-migrate-1.3.0.min.js"></script>
+<script type="text/javascript" src='assets/js/mapbox.js'></script>
+<script type="text/javascript" src='assets/js/leaflet.markercluster.js'></script>
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js"></script>
+<script type="text/javascript" src="assets/js/build.min.js"></script>
+<script type="text/javascript" src="assets/lib/chosen/chosen.jquery.js" ></script>
+<script type="text/javascript" src="assets/js/jquery-ui.js"></script>
+<script type="text/javascript" src="assets/lib/slick/slick.min.js"></script>
+<script type="text/javascript" src="assets/lib/jquerym.menu/js/jquery.mmenu.min.all.js"></script>
+<script type="text/javascript" src="assets/lib/Magnific-Popup-master/jquery.magnific-popup.min.js"></script>
+<script type="text/javascript" src="assets/lib/jQuery.filer-master/js/jquery.filer.min.js"></script>
+<script type="text/javascript" src="assets/js/bootstrap-rating.js"></script>
+<script type="text/javascript" src="assets/lib/popup/js/classie.js"></script> <!-- Popup -->
+<script type="text/javascript" src="assets/lib/popup/js/modalEffects.js"></script> <!-- Popup -->
+<script type="text/javascript" src="assets/js/main.js"></script>
+</body>
+</html>
