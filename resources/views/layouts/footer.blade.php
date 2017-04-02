f9ac6c59586b86f92b2f08241fdbd00c4b127573
leandroargentiero <argentieroleo@gmail.com>
2017-04-02 16:17:41 +0200

    Footer ready

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,51 @@
+<footer class="footer-distributed">
+
+    <div class="footer-left">
+
+        <div class="footer-logo">Apptite logo</div>
+
+        <p class="footer-links">
+            <a href="#">Home</a>
+            ·
+            <a href="#">Over Apptite</a>
+            ·
+            <a href="#">Algemene voorwaarden</a>
+            ·
+            <a href="#">Contact</a>
+        </p>
+
+        <p class="footer-company-name">Apptite &copy; 2017</p>
+    </div>
+
+    <div class="footer-center">
+
+        <div>
+            <i class="fa fa-map-marker"></i>
+            <p><span>Lange Ridderstraat 44</span> Mechelen, België</p>
+        </div>
+
+        <div>
+            <i class="fa fa-envelope"></i>
+            <p><a href="mailto:support@company.com">info@myapptite.be</a></p>
+        </div>
+
+    </div>
+
+    <div class="footer-right">
+
+        <p class="footer-company-about">
+            <span>Over Apptite</span>
+            Apptite is een online platform dat de mogelijkheid biedt om in jouw buurt huisbereide maaltijden bij hobbychefs thuis te gaan eten.
+        </p>
+
+        <div class="footer-icons">
+
+            <a href="#"><i class="fa fa-facebook"></i></a>
+            <a href="#"><i class="fa fa-twitter"></i></a>
+            <a href="#"><i class="fa fa-instagram"></i></a>
+
+        </div>
+
+    </div>
+
+</footer>
