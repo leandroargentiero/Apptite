c5ab84b335c5625aada814dd359acde6eb9feec8
leandroargentiero <argentieroleo@gmail.com>
2017-04-05 20:48:07 +0200

    Created CRUD controllers

--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,20 @@
 Route::get('/', function () {
     return view('home');
 });
-
-
-Auth::routes();
-
+// INDEX - HOME ROUTE
 Route::get('/home', 'HomeController@index');
 
+// /LOGIN - /REGISTER - /PASSWORDRESET
+Auth::routes();
+// LOGOUT ROUTE
 Route::get('logout', '\App\Http\Controllers\Auth\LoginController@logout');
+
+
+
+
+// ALL ROUTES WHERE USER LOGIN IS REQUIRED
+Route::group(['namespace' => 'Admin'], function()
+{
+
+
+});
