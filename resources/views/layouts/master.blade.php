ebe7e8e18d97f10d9225b08caac406e0962b6337
leandroargentiero <argentieroleo@gmail.com>
2017-06-02 15:57:43 +0200

    Fixed typo

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -13,7 +13,7 @@
     <meta property="og:title" content="Apptite"/>
     <meta property="og:description"
           content="Apptite is een online platform dat hobby chefs en foodies samenbrengt voor een verse huisbereide maaltijd."/>
-    <meta property="og:image" content="http://myapptite/assets/images/apptite.jpg"/>
+    <meta property="og:image" content="http://myapptite.be/assets/images/apptite.jpg"/>
 
 
     <!-- Google font-->
