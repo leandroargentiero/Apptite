2327e0d4260ca4109515f2f863b05bb3f04e8e2f
leandroargentiero <argentieroleo@gmail.com>
2017-06-02 17:02:19 +0200

    Updated meta info

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -12,7 +12,7 @@
     <meta property="og:type" content="Website"/>
     <meta property="og:title" content="Apptite | Verse huisbereide maaltijden bij hobby chefs thuis"/>
     <meta property="og:description"
-          content="Apptite is een online platform dat hobby chefs en foodies samenbrengt voor een verse huisbereide maaltijd."/>
+          content="Apptite is een online platform dat hobbychefs en foodies samenbrengt voor een verse huisbereide maaltijd."/>
     <meta property="og:image" content="http://myapptite.be/assets/images/apptite.jpg"/>
 
 
