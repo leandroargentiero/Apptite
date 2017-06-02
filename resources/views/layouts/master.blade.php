91073e2cf833912d7cda319780777f5d96cea822
leandroargentiero <argentieroleo@gmail.com>
2017-06-02 15:32:31 +0200

    Updated meta tags

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,16 +2,17 @@
 <html lang="en">
 <head>
 
-    <title>Apptite - Het geheime ingrediënt voor een gloednieuwe eetervaring</title>
+    <title>Apptite - Het geheime ingredient voor een gloednieuwe eetervaring</title>
 
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <meta property="og:url" content="http://myapptite.be"/>
-    <meta property="og:type" content="Article"/>
+    <meta property="og:type" content="Website"/>
     <meta property="og:title" content="Apptite"/>
-    <meta property="og:description" content="Apptite is een online platform dat hobby chefs en foodies samenbrengt voor een huisbereide maaltijd."/>
+    <meta property="og:description"
+          content="Apptite is een online platform dat hobby chefs en foodies samenbrengt voor een verse huisbereide maaltijd."/>
     <meta property="og:image" content="http://myapptite/assets/images/apptite.jpg"/>
 
 
@@ -20,7 +21,8 @@
     <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
     <!-- CSS & Bootstrap-->
     <link href="/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.min.css">
+    <link rel="stylesheet" type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="/css/app.css">
     <link rel="stylesheet" type="text/css" href="/css/hover-min.css">
@@ -63,10 +65,17 @@
 
 <!-- Analytics script -->
 <script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    (function (i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function () {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
     ga('create', 'UA-57911115-3', 'auto');
     ga('send', 'pageview');
