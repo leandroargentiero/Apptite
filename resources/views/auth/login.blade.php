ab629ea68290a27bd5420f3deec15705482c04dd
leandroargentiero <argentieroleo@gmail.com>
2017-06-06 18:51:16 +0200

    Update the login screen with new layout

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,117 +1,54 @@
-@extends('layouts.master')
+@extends('layouts.master_stripped')
+<div class="wrapper-login">
+    <div class="col-md-6 col-md-offset-3">
+        <div class="panel panel-default">
+            <div class="panel-body">
 
-@section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-6 col-md-offset-3">
-                <div class="panel panel-default">
-                    <div class="panel-body">
+                <img id="logo-login" src="/assets/images/logo_apptite_black.svg" alt="logo apptite">
 
-                        <img id="logo-login" src="/assets/images/logo_apptite_black.svg" alt="logo apptite">
-                        
-                        <form role="form" method="POST" action="{{ route('login') }}">
-                            {{ csrf_field() }}
+                <form role="form" method="POST" action="{{ route('login') }}">
+                    {{ csrf_field() }}
 
-                            <div class="form-group col-md-12 {{ $errors->has('email') ? ' has-error' : '' }}">
-                                <form role="form">
-                                    <div class="form-group float-label-control">
-                                        <label for="email">Email</label>
-                                        <input id="email" name="email" type="email" class="form-control"
-                                               placeholder="Email" value="{{ old('email') }}">
-                                    </div>
-                                    @if ($errors->has('email'))
-                                        <span class="help-block">
+                    <div class="form-group col-md-12 {{ $errors->has('email') ? ' has-error' : '' }}">
+                        <form role="form">
+                            <div class="form-group float-label-control">
+                                <label for="email">Email</label>
+                                <input id="email" name="email" type="email" class="form-control"
+                                       placeholder="Email" value="{{ old('email') }}">
+                            </div>
+                            @if ($errors->has('email'))
+                                <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
-                                    @endif
+                            @endif
 
-                                    <div class="form-group float-label-control">
-                                        <label for="">Wachtwoord</label>
-                                        <input name="password" type="password" class="form-control"
-                                               placeholder="Wachtwoord" id="password" value="{{ old('password') }}">
-                                    </div>
-                                    @if ($errors->has('password'))
-                                        <span class="help-block">
+                            <div class="form-group float-label-control">
+                                <label for="">Wachtwoord</label>
+                                <input name="password" type="password" class="form-control"
+                                       placeholder="Wachtwoord" id="password" value="{{ old('password') }}">
+                            </div>
+                            @if ($errors->has('password'))
+                                <span class="help-block">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
-                                    @endif
-                                </form>
-
-                                <div class="">
-                                    <button type="submit" class="btn btn-primary">
-                                        Inloggen
-                                    </button>
-                                </div>
-
-                                <div class="pull-right">
-                                    <a href="{{ url('/register') }}">Nog geen account? Maak hier een nieuw account
-                                        aan.</a>
-                                </div>
-
-                            </div>
-
-                            <!--
-
-                            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                                <label for="email" class="col-md-4 control-label">E-mail</label>
-
-                                <div class="col-md-6">
-                                    <div class="input-group">
-                                        <span class="input-group-addon"><i class="fa fa-envelope"
-                                                                           aria-hidden="true"></i></span>
-                                        <input id="email" type="email" class="form-control" name="email"
-                                               value="{{ old('email') }}" required autofocus>
-                                    </div>
-                                    @if ($errors->has('email'))
-                                    <span class="help-block">
-                                    <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                    @endif
-                                    </div>
-                                </div>
-
-                                <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                                <label for="password" class="col-md-4 control-label">Wachtwoord</label>
-
-                                <div class="col-md-6">
-                                    <div class="input-group">
-                                        <span class="input-group-addon"><i class="fa fa-key"
-                                                                           aria-hidden="true"></i></span>
-                                        <input id="password" type="password" class="form-control" name="password"
-                                               required>
-                                    </div>
-
-                                    @if ($errors->has('password'))
-                                    <span class="help-block">
-                                    <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                    @endif
-                                    </div>
-                                </div>
-
-
-                                <div class="form-group">
-                                    <div class="col-md-6 col-md-offset-4">
-                                        <div>
-                                            <a href="{{ url('/register') }}">Nog geen account? Maak hier een nieuw account
-                                            aan.</a>
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="form-group">
-                                <div class="col-md-8 col-md-offset-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        Inloggen
-                                    </button>
-
-                                </div>
-                            </div>
-                            -->
+                            @endif
                         </form>
+
+                        <div class="">
+                            <button type="submit" class="btn btn-primary">
+                                Inloggen
+                            </button>
+                        </div>
+
+                        <div class="pull-right">
+                            <a href="{{ url('/register') }}">Nog geen account? Maak hier een nieuw account
+                                aan.</a>
+                        </div>
+
                     </div>
-                </div>
+                </form>
             </div>
         </div>
     </div>
-@endsection
+</div>
+
---
+++ resources/views/layouts/master_stripped.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <title>Apptite | Verse huisbereide maaltijden bij hobbychefs thuis</title>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta property="og:url" content="http://myapptite.be"/>
+    <meta property="og:type" content="Website"/>
+    <meta property="og:title" content="Apptite | Verse huisbereide maaltijden bij hobbychefs thuis"/>
+    <meta property="og:description"
+          content="Apptite is een online platform dat foodies samenbrengt voor een verse huisbereide maaltijd bij hobbychefs thuis."/>
+    <meta property="og:image" content="http://myapptite.be/assets/images/apptite.jpg"/>
+
+
+    <!-- Google font-->
+    <link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+    <!-- CSS & Bootstrap-->
+    <link href="/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="/css/app.css">
+    <link rel="stylesheet" type="text/css" href="/css/hover-min.css">
+    <link rel="stylesheet" type="text/css" href="/css/animate.css">
+
+    <!-- Ajax -->
+    <meta name="_token" content="{!! csrf_token() !!}"/>
+
+    <!-- Favicon -->
+    <link rel="icon" href="/favicon.ico" type="image/x-icon"/>
+
+</head>
+<body>
+
+<section>
+    @yield('login')
+</section>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script src="/assets/bootstrap/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/js/bootstrap-datetimepicker.min.js"></script>
+<script type="text/javascript">
+    $.ajaxSetup({
+        headers: {'X-CSRF-Token': $('meta[name=_token]').attr('content')}
+    });
+</script>
+
+<!-- Analytics script -->
+<script>
+    (function (i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function () {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+
+    ga('create', 'UA-57911115-3', 'auto');
+    ga('send', 'pageview');
+
+</script>
+
+<script src="/js/all.js"></script>
+
+</body>
+</html>
